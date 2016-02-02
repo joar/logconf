d0295d78bb48cccd0912c5a8ed628ec19c532f43
Joar Wandborg <joar@wandborg.se>
2016-02-02 07:54:26 +0100

    Add stuff

--- src/logconf/__init__.py
+++ src/logconf/__init__.py
@@ -1,13 +1,23 @@
-from .dsl import DSLBase
+import logging
+from logconf.dsl import DSLBase
+from logconf.dsl import Loggers, Logger, RootLogger, Handlers, Handler
+
+_log = logging.getLogger(__name__)
 
 
 def logconf(dsl_expression):
-    return to_dict_recursive(dsl_expression)
+    config = {
+        'version': 1
+    }
+    config.update(to_dict_recursive(dsl_expression))
+    return config
 
 
 def to_dict_recursive(start):
+    _log.debug('to_dict_recursive: start=%r', start)
     if isinstance(start, (DSLBase, dict)):
         if isinstance(start, DSLBase):
+            _log.debug('is DSLBaser: %r', start)
             start = start.to_dict()
 
         return {k: to_dict_recursive(v) for k, v in start.items()}
@@ -16,3 +26,36 @@ def to_dict_recursive(start):
     else:
         return start
 
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    import json
+    import logging.config
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    a = logconf(
+        Handlers(
+            Handler('logging.StreamHandler', logging.DEBUG, 'verbose',
+                    'verbose'),
+            console=Handler(
+                klass='logging.StreamHandler'
+            )
+        ) +
+        RootLogger(
+            logging.DEBUG,
+            ['console']
+        ) +
+        Loggers(
+            Logger('logconf.dsl', logging.DEBUG, ['console']),
+            foo=Logger(level=logging.DEBUG, handlers=['console'])
+        )
+    )
+
+    #logging.config.dictConfig(a)
+
+    print(json.dumps(a, indent=4
+                     ))
+
+    pprint(a)
--- src/logconf/dsl.py
+++ src/logconf/dsl.py
@@ -1,3 +1,8 @@
+import logging
+
+_log = logging.getLogger(__name__)
+
+
 class DotDict(dict):
     def __getattr__(self, item):
         return self[item]
@@ -6,8 +11,12 @@ class DotDict(dict):
         self[key] = value
 
     def __add__(self, other):
+        _log.debug('DotDict.__add__: self=%r, other=%r', self, other)
         self.update(other)
         return self
+
+    def __repr__(self):
+        return '<DotDict {0}>'.format(super(DotDict, self).__repr__())
 
 
 class DSLBase:
@@ -17,16 +26,23 @@ class DSLBase:
         self.data = DotDict()
 
     def to_dict(self):
+        _log.debug('%s.to_dict: self.data=%r',
+                   self.__class__.__name__,
+                   self.data)
         if self.wrap_with_key is not None:
-            return {self.wrap_with_key: self.data}
+            _log.debug('self.wrap_with_key=%r', self.wrap_with_key)
+            return DotDict({self.wrap_with_key: self.data})
+
+        assert isinstance(self.data, DotDict)
 
         return self.data
 
     def __add__(self, other):
+        _log.debug('__add__: %r + %r', self.to_dict(), other.to_dict())
         return self.to_dict() + other.to_dict()
 
     def __repr__(self):
-        return '<{} {}>'.format(self.__class__.__name__,
+        return '<{} data={}>'.format(self.__class__.__name__,
                                 self.to_dict())
 
 
@@ -41,30 +57,75 @@ class LogConf(DSLBase):
                           version=version)
 
 
-class Logger(DSLBase):
-    def __init__(self, name, level, handlers):
+class WrapWithNameMixin:
+    def __init__(self):
+        self.wrapping_disabled = False
+
+    def disable_wrap(self, disable):
+        _log.debug('%s.disable_wrap(%r): self=%r',
+                   self.__class__.__name__,
+                   disable,
+                   self)
+        self.wrapping_disabled = disable
+        return self
+
+    @property
+    def wrap_with_name(self):
+        if self.wrapping_disabled:
+            return None
+
+        return self.data.get('name')
+
+
+class ItemWrapper(DSLBase):
+    def __init__(self, *args, **kwargs):
+        _log.debug('%s.__init__(args=%r, kwargs=%r)',
+                   self.__class__.__name__,
+                   args,
+                   kwargs)
+        super(ItemWrapper, self).__init__()
+
+        for arg in args:
+            self.data += arg.to_dict()
+
+        self.data += {k: v.disable_wrap(True) for k, v in kwargs.items()}
+
+
+class Handlers(ItemWrapper):
+    wrap_with_key = 'handlers'
+
+
+class Loggers(ItemWrapper):
+    wrap_with_key = 'loggers'
+
+
+class Logger(DSLBase, WrapWithNameMixin):
+    def __init__(self, name=None, level=None, handlers=None):
         super(Logger, self).__init__()
         self.data += dict(name=name,
                           level=level,
                           handlers=handlers)
 
 
-class Handlers(DSLBase):
-    wrap_with_key = 'handlers'
-
-    def __init__(self, **kwargs):
-        super(Handlers, self).__init__()
-        self.data += kwargs
+class RootLogger(Logger):
+    def __init__(self, level, handlers):
+        name = 'root'
+        super(RootLogger, self).__init__(name, level, handlers)
 
 
-class Handler(DSLBase):
-    def __init__(self, klass=None, level=None, formatter=None):
+class Handler(DSLBase, WrapWithNameMixin):
+    def __init__(self, klass=None, level=None, formatter=None, name=None):
         super(Handler, self).__init__()
         self.data += {'class': klass}
         self.data += dict(
             level=level,
-            formatter=formatter
+            formatter=formatter,
+            name=None
         )
+
+    @property
+    def wrap_with_key(self):
+        return self.data.get('name')
 
 
 
